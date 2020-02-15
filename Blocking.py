a34edc8e79df69d44cff4fd641001f0eff1b911e
Joonas Toimela <joonas.toimela@gmail.com>
2020-02-15 23:09:58 +0200

    Add blocking evaluation

--- Blocking.py
+++ Blocking.py
@@ -8,7 +8,8 @@ from nltk.corpus import stopwords
 stop = stopwords.words('english')
 lancaster = LancasterStemmer()
 
-def titleTokenizer(EC):
+# Maybe define these dataset specific function in main instead or make them generic
+def AmazonGoogletitleTokenizer(EC):
     #Title is always second column in these datasets
     titles = EC[:,1]
     stemmed = []
@@ -19,7 +20,7 @@ def titleTokenizer(EC):
     return stemmed
 
 
-def tokenBlocker(tokenArray):
+def TokenBlocker(tokenArray):
     token_dictionary = {}
     for index, tokens in enumerate(tokenArray):
         for token in tokens:
@@ -28,7 +29,9 @@ def tokenBlocker(tokenArray):
             token_dictionary[token] = a
     return(token_dictionary)
 
-def joinBlocks(BC1, BC2):
+
+
+def JoinBlocks(BC1, BC2):
     combined = {}
     for key in BC1:
         if(key in BC2):
@@ -42,7 +45,46 @@ def TokenBlocking(EntityCollection1, EntityCollection2, transformationFun, const
     blockedEC1 = constraintFun(transformedEC1)
     blockedEC2 = constraintFun(transformedEC2)
 
-    blockCollection = joinBlocks(blockedEC1, blockedEC2)
+    blockCollection = JoinBlocks(blockedEC1, blockedEC2)
+    return(blockCollection)
 
-    duplicates = matchFun(blockCollection)
-    return(duplicates)
+
+def _goldStandardToIndexArray(EntityCollection1, EntityCollection2, goldStandard):
+    #Extracts all the required comparisons from the goldStandard dataset as list of tuples
+    s = goldStandard.shape
+    N = s[0]
+    goldStandardIndices = np.ndarray(shape=s, dtype=np.int32)
+    pk1 = EntityCollection1[:, 0]
+    pk2 = EntityCollection2[:, 0]
+    for i in np.arange(N):
+        # Those are supposed to be primary keys so there should ever be only one match
+        goldStandardIndices[i, 0] =  np.where(pk1 == goldStandard[i, 0])[0]
+        goldStandardIndices[i, 1] =  np.where(pk2 == goldStandard[i, 1])[0]
+    return(list(map(tuple, goldStandardIndices)))
+
+def _reduceBlockToComparisons(block):
+    comparisons = []
+    part1 = block[0]
+    part2 = block[1]
+    for item in part1:
+        for item2 in part2:
+            comparisons.append((item, item2))
+    return(comparisons)
+
+def _reduceBlockCollectionToComparisons(bc):
+    comparisons = [_reduceBlockToComparisons(block) for key, block in bc.items()]
+    comparisons =  [item for sublist in comparisons for item in sublist]
+    return(comparisons)
+
+
+def EvaluateBlockCollection(EntityCollection1, EntityCollection2, blockCollection, goldStandard):
+    gsIndexArray = _goldStandardToIndexArray(EntityCollection1, EntityCollection2, goldStandard)
+    comparisons = _reduceBlockCollectionToComparisons(blockCollection)
+    unique_comparisons = set(comparisons)
+    matches = [comparison for comparison in gsIndexArray if comparison in unique_comparisons]
+    pc = len(matches)/len(gsIndexArray)*100
+    rr1 = (1 - len(comparisons)/(len(EntityCollection1)*len(EntityCollection2)))*100
+    rr2 = (1 - len(unique_comparisons)/(len(EntityCollection1)*len(EntityCollection2)))*100
+    print("Pair Completenes: {0}%, Reduction Ratio: {1}%, Reduction Ratio (with redundant pruned): {2}%".format(pc, rr1,rr2 ))
+
+
